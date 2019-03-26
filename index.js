b587a6743f6d3cb26c0630a6bba23088c89d2268
Jared <jaredbolduc@yahoo.com>
2019-03-25 21:13:02 -0600

    updated from class

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-console.log('connected')
-
 var buttons = document.getElementsByClassName('button')
-var scoreKeeper
+var scoreKeeper 
 var quitButton
 var simonsArray = []
+var playersArray = []
+var counter = 0
 var whosTurnIsIt = 'computers'
+
+for(let i = 0; i<buttons.length; i++){
+    buttons[i].addEventListener('click', handleButton(buttons[i].id))
+}
 
 randomColorGenerator()
 
-for( let i = 0; i <buttons.length; i++) {
-
-    this.addEventListener('click', handleButton)
-}
-
-function handleButton(event) {
-
-}
-function randomColorGenerator () {
-    var randomButton = buttonsbuttons[Math.trunc(Math.random()*4)]
-    randomButton.classList.toggle('border')
-    setTimeout(() => {
-        randomButton.classList.toggle ('boarder')
-    }, 1000)
-    simonsArray.push(randomButton.id)
-    handlePlayerTurns(whosTurnIsIt)
-}
-
-function handlePlayerTurns(whosTurn) {
-    if(whosTurn === 'computers'){
-        whosTurnIsIt = 'player'
-    } else {
-        whosTurnIsIt = 'computers'
+function handleButton(id) {
+    return function (){
+        console.log(simonsArray[counter].id)
+        if (id === simonsArray[counter].id) {
+            playersArray.push(id)
+            console.log(playersArray)
+            counter++
+            handlePlayerTurns()
+            randomColorGenerator()
+        } else {
+            counter = 0
+            console.log('game over')
+        }
     }
 }
+
+function randomColorGenerator () {
+    if (whosTurnIsIt === 'computers') {
+        var randomButton = buttons[Math.trunc(Math.random()*4)]
+        simonsArray.push(randomButton)
+        simonsArray.forEach(DOMbutton => {
+
+            document.getElementById(DOMbutton.id).classList.toggle('border')
+            console.log('randomButton', randomButton)
+            setTimeout(()=> {
+                randomButton.classList.toggle('border')
+            }, 1000)
+            handlePlayerTurns()
+        })
+    }
+}
+
+function handlePlayerTurns(){
+    if(whosTurnIsIt === 'computers'){
+        whosTurnIsIt = 'player'
+        console.log('players turn')
+    } else {
+        whosTurnIsIt = 'computers'
+        console.log('computers turn')
+    }
+}
+// after color its players turn
+
+
+
+
